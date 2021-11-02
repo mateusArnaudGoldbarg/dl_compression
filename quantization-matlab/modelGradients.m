2a549aee471ce08c5927b90bd45e54529137589b
mateusArnaudGoldbarg <42541080+mateusArnaudGoldbarg@users.noreply.github.com>
2021-11-02 20:06:46 -0300

    Add files via upload

---
+++ quantization-matlab/modelGradients.m
@@ -0,0 +1,7 @@
+function [gradients,state,loss] = modelGradients(dlnet,dlX,Y)
+
+[dlYPred,state] = forward(dlnet,dlX);
+loss = crossentropy(dlYPred,Y);
+gradients = dlgradient(loss,dlnet.Learnables);
+
+end
